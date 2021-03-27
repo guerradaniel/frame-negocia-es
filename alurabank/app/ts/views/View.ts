9a736561bda246e3ba6775954ae873298fdade7d
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-03-26 22:54:18 -0300

    Adicionei generics ao código para facilitar a chamada do método.

--- alurabank/app/ts/views/View.ts
+++ alurabank/app/ts/views/View.ts
@@ -1,10 +1,18 @@
-class View {
+class View<T>{
 
-    protected _elemento: Element
+    private _elemento: Element
 
     constructor(seletor: string) {
         this._elemento = document.querySelector(seletor)
     }
 
+    update(model: T) {
+        this._elemento.innerHTML = this.template(model)
+    }
+
+    template(model: T): string {
+
+        throw new Error('Você deve implementar o método template')
+    }
 
 }
