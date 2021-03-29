fb90dedd802b97a0a24f8ebb5be778e2a6afd899
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-03-29 00:54:36 -0300

    Usei enum para bloquear cadastro em finais de semana.

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -21,8 +21,15 @@ export class NegociacaoController {
     adiciona(event: Event) {
         event.preventDefault()
 
+        let data = new Date(this._inputData.val().replace(/-/g, ','))
+
+        if (!this._ehDiaUtil(data)) {
+            this._mensagemView.update('Somente negociações em dias úteis, por favor!')
+            return
+        }
+
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g, ',')),
+            data,
             parseInt(this._inputQuantidade.val()),
             parseInt(this._inputValor.val())
         )
@@ -30,6 +37,19 @@ export class NegociacaoController {
         this._negociacoes.adiciona(negociacao)
         this._negociacoesView.update(this._negociacoes)
         this._mensagemView.update('Negociação adicionada com sucesso');
-
     }
+
+    private _ehDiaUtil(data: Date) {
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo
+    }
+}
+
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
 }
